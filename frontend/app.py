7b84c56fd880cc5204e782a2ab20604a731f647f
Ashish Soni <ashish.soni2091@gmail.com>
2024-08-24 03:14:20 +0200

    gradio app

---
+++ frontend/app.py
@@ -0,0 +1,11 @@
+from dotenv import dotenv_values
+import gradio as gr
+import os
+
+config = dotenv_values("/frontend/.env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+
+## LANGSMITH(by Langchain)
+LANGCHAIN_TRACING_V2=True
+LANGCHAIN_ENDPOINT=config["LANGSMITH_API_URL"]
+LANGSMITH_API_KEY = config["LANGSMITH_API_KEY"]
+LANGSMITH_PROJECT = config["LANGSMITH_PROJECT"]
