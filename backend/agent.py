33939b16c7df1270999c7a07afad9fb6e34aa30e
Ashish Soni <ashish.soni2091@gmail.com>
2024-08-24 03:14:06 +0200

    backend

---
+++ backend/agent.py
@@ -0,0 +1,9 @@
+from dotenv import dotenv_values
+
+config = dotenv_values("/backend/.env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+
+## WATSONX
+WATSONX_API_KEY = config["WATSONX_API_KEY"]
+WATSONX_URL = config["WATSONX_API_URL"]
+WATSONX_PROJECT_ID = config["WATSONX_PROJECT_ID"]
+
